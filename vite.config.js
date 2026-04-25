29241a785626a02b59d11f1b3cc119326887b139
Hassan <dev1@4secu.com>
2026-04-25 18:34:47 +0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
-  base: '/AquaSecureAI/',  // This must match your repository name
+  // server: { port: 3000 },
+  base: '/',  // This must match your repository name
 })
