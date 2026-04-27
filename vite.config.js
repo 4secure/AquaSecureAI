256c9bcf56eefff9a85f4424a4e7cb232a4dfb3c
abdulhadiabroo <abdulhadiabroo@gmail.com>
2026-04-27 23:21:38 +0500

    reload issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  base: '/',  // This must match your repository name
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
